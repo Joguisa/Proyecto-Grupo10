d9279933a115ae78010f2543700e331e652870dc
Jonatán Guillén <jsglln@hotmail.com>
2023-08-11 21:25:04 -0500

    mostrar información de película individual

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,6 +1,8 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:proyecto_grupo10/domain/entities/movie.dart';
+import 'package:proyecto_grupo10/presentation/providers/actors/actors_by_movie_provider.dart';
 import 'package:proyecto_grupo10/presentation/providers/movies/movie_detail_provider.dart';
 
 class MovieScreen extends ConsumerStatefulWidget {
@@ -25,6 +27,7 @@ void initState() {
   super.initState();
 
   ref.read(movieDetailProdiver.notifier).loadMovie(widget.movieId);
+  ref.read(actorsByMovieProvider.notifier).loadActors(widget.movieId);
   
 }
 
@@ -121,10 +124,75 @@ class _MovieDetails extends StatelessWidget {
         //   ),
         // ),
 
-        // TODO Mostrar actores ListView
+        // Mostrar actores ListView
+        _ActorsByMovie(movieId: movie.id.toString() ),
 
-        const SizedBox(height: 100),
+        const SizedBox(height: 50),
       ],
+    );
+  }
+}
+
+class _ActorsByMovie extends ConsumerWidget {
+
+  final String movieId;
+
+  const _ActorsByMovie({required this.movieId});
+
+  @override
+  Widget build(BuildContext context, ref) {
+
+    final actorsByMovie = ref.watch( actorsByMovieProvider );
+
+    if (actorsByMovie[movieId] == null) {
+      return const CircularProgressIndicator.adaptive(strokeWidth: 2);
+    }
+
+    final actors = actorsByMovie[movieId]!;
+
+    return SizedBox(
+      height: 300,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: actors.length,
+        itemBuilder: (BuildContext context, int index) {
+          final actor = actors[index];
+
+          return Container(
+            padding: const EdgeInsets.all(8.0),
+            width: 135,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                
+                // Cast Photos
+                FadeInRight(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.network(
+                      actor.profilePath,
+                      height: 180,
+                      width: 135,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+
+                // Cast names
+                const SizedBox(height: 5),
+                Text(actor.name, maxLines: 2),
+                Text(actor.character ?? '', 
+                maxLines: 2,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  overflow: TextOverflow.ellipsis
+                ),
+                ),
+              ]
+            ),
+          );
+        },
+      ),
     );
   }
 }
@@ -139,7 +207,9 @@ class _CustomSliverAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     final size = MediaQuery.of(context).size;
+
     return SliverAppBar(
       backgroundColor: Colors.black,
       expandedHeight: size.height * 0.7,
@@ -147,17 +217,22 @@ class _CustomSliverAppBar extends StatelessWidget {
       // shadowColor: Colors.red, // comentar si se ve mal 
       flexibleSpace: FlexibleSpaceBar(
         titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-        title: Text(
-          movie.title,
-          style: const TextStyle(fontSize: 20),
-          textAlign: TextAlign.start,
-        ),
+        // title: Text(
+        //   movie.title,
+        //   style: const TextStyle(fontSize: 20),
+        //   textAlign: TextAlign.start,
+        // ),
         background: Stack(
           children: [
+
             SizedBox.expand(
               child: Image.network(
                 movie.posterPath,
-                fit: BoxFit.cover
+                fit: BoxFit.cover,
+                // loadingBuilder: (context, child, loadingProgress) {
+                //   if ( loadingProgress != null ) return const SizedBox();
+                //   return FadeIn(child: child);
+                // },
               ),
             ),
 
