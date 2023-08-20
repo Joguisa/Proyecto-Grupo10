0813bf8bc3f5f44fb45c8efdb10106b2ed7ec6fd
Jonatán Guillén <jsglln@hotmail.com>
2023-08-20 15:53:31 -0500

    finalización de la navegación y archivo de rutas

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,29 +1,28 @@
 import 'package:go_router/go_router.dart';
 import 'package:proyecto_grupo10/presentation/screens/screens.dart';
-late final Map<String, dynamic> params;
+
 final appRouter = GoRouter(
-  
-  initialLocation: '/',
+  initialLocation: '/home/0', 
   routes: [
     GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
-      routes: [
+        path: '/home/:page',
+        name: HomeScreen.name,
+        builder: (context, state) {
+          final pageIndex = int.parse(state.pathParameters['page'] ?? '0');
+          return HomeScreen(pageIndex: pageIndex);
+        },
+        routes: [
+          GoRoute(
+              path: 'movie/:id',
+              name: MovieScreen.name,
+              builder: (context, state) {
+                final movieId = state.pathParameters['id'] ?? 'no-id';
+                return MovieScreen(movieId: movieId);
+              }),
+        ]),
+
         GoRoute(
-      path: 'movie/:id',
-      name: MovieScreen.name,
-      builder: (context, state) {
-        final movieId = state.pathParameters['id'] ?? 'no-id';
-
-        return MovieScreen( movieId: movieId);
-      }
-    ),
-      ]
-    ),
-
-    
-
-    
-  ]
-);
+          path: '/',
+          redirect: ( _, __ ) => '/home/0',
+        ),
+]);
